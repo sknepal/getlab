91d8e56b7810eceb6ba463af688a5c66c360f782
Subigya Nepal <subigya.nepal@deerwalk.edu.np>
2019-04-08 02:56:45 -0400

    first

---
+++ README.md
@@ -0,0 +1,26 @@
+# Intro
+Simple script to clone all Gitlab repos (including private ones) that you have access to. 
+
+# Requirements
+Uses os, subprocess, python-gitlab, sys, shutil on python 3.
+```
+pip3 install python-gitlab
+```
+
+* Other packages should be installed/available by default.
+
+# Setup
+It uses gitlab API, so you need to get access tokens by going to Settings > Access tokens. Select api, read_user, read_repository checkboxes under scopes. Click on Create personal access token. 
+
+* Replace **YOUR_PRIVATE_TOKEN** in the code with your access token.
+* Replace **gitlab_url** variable's value with your gitlab server URL.
+
+# Use
+```
+python3 getlab.py [username] [password] [optional path]
+```
+* *username and password* are the login combination that you use to login to the gitlab server. 
+
+* *optional_path* is the absolute path of the destination directory to clone the repositories to. If not provided, it will use the current path.
+
+
---
+++ getlab.py
@@ -0,0 +1,49 @@
+##Subigya Nepal
+##Mon Apr 8, 2019, 2.37 AM
+import os, subprocess, gitlab, sys, shutil
+  
+def main():
+    gitlab_url = 'https://gitlab.cs.dartmouth.edu' #enter your custom gitlab url
+    private_token = 'YOUR_PRIVATE_TOKEN' #settings > access tokens > api, read_user, read_repo > create
+    
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        path = '.'
+    elif len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        path = sys.argv[3] ##absolute path
+    else:
+        print("Please pass arguments in this format: username password optional_absolute_path.\nAt least two arguments required.")
+        sys.exit(0)
+
+    try:
+        auth_obj = git_auth(gitlab_url, private_token)
+        give_me_repos(auth_obj, username, password, path)
+    except Exception as e:
+        print(e)
+        print("\nAre you sure you provided correct username/password/token combination?\n")
+        
+
+def git_auth(gitlab_url, private_token):
+    gl = gitlab.Gitlab(gitlab_url, private_token=private_token, api_version=4)
+    gl.auth()
+    print("Authenticated via Token")
+    print("=============\n")
+    return gl
+
+def give_me_repos(gl, username, password, path):
+    progress = 0
+    projects = gl.projects.list()
+    for project in projects:
+        dir_name = project.path
+        if os.path.exists(os.path.join(path, dir_name)):
+            shutil.rmtree(os.path.join(path, dir_name))
+        git_url = 'https://' + username + ':' + password + '@' + project.http_url_to_repo.split("https://")[1]
+        do_your_thing = subprocess.call(['git', 'clone', git_url], cwd=path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        progress+= 1
+        print('Progress: {}/{} repos processed'.format(progress, len(projects)))
+        
+if __name__ == "__main__":
+    main()
